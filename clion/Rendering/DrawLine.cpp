ee6ddff001f3b0a2b073b4e99212fb8b6963ed7a
aje00009 <aje00009@red.ujaen.es>
2026-02-13 14:41:12 +0100

    Implementación Line y Ray

--- clion/Rendering/DrawLine.cpp
+++ clion/Rendering/DrawLine.cpp
@@ -5,19 +5,23 @@
 
 AlgGeom::DrawLine::DrawLine (Line &line): Model3D(), _line(line)
 {
-	Vect2d slope(line.getB().getX() - line.getA().getX(), line.getB().getY() - line.getA().getY());
+	//Usamos una t alta para simular la recta infinita
+	Point pStart = line.getPoint(-1000.0);
+	Point pEnd   = line.getPoint(1000.0);
 
 	Component* component = new Component;
-    component->_vertices.insert(component->_vertices.end(), {
-			VAO::Vertex { vec3(line.getA().getX() - slope.getX() * 1000.0f, line.getA().getY() - slope.getY() * 1000.0f, .0f) },
-			VAO::Vertex { vec3(line.getB().getX() + slope.getX() * 1000.0f, line.getB().getY() + slope.getY() * 1000.0f, .0f) }
-        });
 
-    component->_indices[VAO::IBO_LINE].insert(component->_indices[VAO::IBO_LINE].end(), { 0, 1 });
-    this->_components.push_back(std::unique_ptr<Component>(component));
+	// Insertamos los vértices calculados
+	component->_vertices.insert(component->_vertices.end(), {
+		  VAO::Vertex { vec3((float)pStart.getX(), (float)pStart.getY(), .0f) },
+		  VAO::Vertex { vec3((float)pEnd.getX(),   (float)pEnd.getY(),   .0f) }
+		});
 
-    component->completeTopology();
+	component->_indices[VAO::IBO_LINE].insert(component->_indices[VAO::IBO_LINE].end(), { 0, 1 });
+	this->_components.push_back(std::unique_ptr<Component>(component));
 
-    this->calculateAABB();
-    this->buildVao(component);
+	component->completeTopology();
+
+	this->calculateAABB();
+	this->buildVao(component);
 }
--- clion/Rendering/DrawRay.cpp
+++ clion/Rendering/DrawRay.cpp
@@ -5,10 +5,13 @@
 
 AlgGeom::DrawRay::DrawRay (RayLine &ray): Model3D(), _ray(ray)
 {
+    Point pEnd = ray.getPoint(1000.0);
+
     Component* component = new Component;
     component->_vertices.insert(component->_vertices.end(), {
-            VAO::Vertex { vec3(ray.getA().getX(), ray.getA().getY(), .0f) },
-            VAO::Vertex { vec3(ray.getB().getX() * 1000.0f, ray.getB().getY() * 1000.0f, .0f) }
+            VAO::Vertex { vec3((float)ray.getA().getX(), (float)ray.getA().getY(), .0f) },
+
+            VAO::Vertex { vec3((float)pEnd.getX(), (float)pEnd.getY(), .0f) }
         });
 
     component->_indices[VAO::IBO_LINE].insert(component->_indices[VAO::IBO_LINE].end(), { 0, 1 });
