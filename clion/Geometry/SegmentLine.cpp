75aac48526f74d10a775ad80867bb4321d0a887d
aje00009 <aje00009@red.ujaen.es>
2026-02-13 14:41:05 +0100

    Implementación SegmentLine

--- clion/Geometry/SegmentLine.cpp
+++ clion/Geometry/SegmentLine.cpp
@@ -39,14 +39,26 @@ SegmentLine & SegmentLine::operator=(const SegmentLine & segment)
 
 bool SegmentLine::segmentIntersection(SegmentLine& l)
 {
-	// XXXX
-	return false;
+	Point a = this->_orig;
+	Point b = this->_dest;
+	Point c = l._orig;
+	Point d = l._dest;
+
+	if (a.colinear(c,d) || b.colinear(c,d) ||
+		c.colinear(a,b) || d.colinear(a,b))
+		return false;
+	return (a.left(c,d) ^ b.left(c,d) &&
+		c.left(a,b) ^ d.left(a,b));
 }
 
 double SegmentLine::getC()
 {
-	// XXXX
-	return 0.0;
+	if (isVertical())
+		return std::numeric_limits<double>::infinity();
+
+	double m = slope();
+
+	return _orig.getY() - m * _orig.getX();
 }
 
 bool SegmentLine::distinct(SegmentLine & segment)
@@ -66,32 +78,67 @@ bool SegmentLine::equal(SegmentLine & segment)
 
 Point SegmentLine::getPoint(double t)
 {
-    // XXXX
-	return Point();
+    double x = _orig.getX() + t * (_dest.getX() - _orig.getX());
+	double y = _orig.getY() + t * (_dest.getY() - _orig.getY());
+
+	return Point(x, y);
 }
 
 bool SegmentLine::impSegmentIntersection(SegmentLine& segment)
 {
-	// XXXX
-    return true;
+	Point a = this->_orig;
+	Point b = this->_dest;
+	Point c = segment._orig;
+	Point d = segment._dest;
+
+	Point::PointClassification cl;
+
+	// 1. ¿Está C contenido en AB?
+	cl = c.classify(a, b);
+	if (cl == Point::PointClassification::BETWEEN ||
+		cl == Point::PointClassification::ORIGIN ||
+		cl == Point::PointClassification::DEST) return true;
+
+	// 2. ¿Está D contenido en AB?
+	cl = d.classify(a, b);
+	if (cl == Point::PointClassification::BETWEEN ||
+		cl == Point::PointClassification::ORIGIN ||
+		cl == Point::PointClassification::DEST) return true;
+
+	// 3. ¿Está A contenido en CD?
+	cl = a.classify(c, d);
+	if (cl == Point::PointClassification::BETWEEN ||
+		cl == Point::PointClassification::ORIGIN ||
+		cl == Point::PointClassification::DEST) return true;
+
+	// 4. ¿Está B contenido en CD?
+	cl = b.classify(c, d);
+	if (cl == Point::PointClassification::BETWEEN ||
+		cl == Point::PointClassification::ORIGIN ||
+		cl == Point::PointClassification::DEST) return true;
+
+	return false;
 }
 
 bool SegmentLine::isHorizontal()
 {
-	// XXXX
-	return true;
+	return BasicGeometry::equal(this->_orig.getY(), this->_dest.getY());
 }
 
 bool SegmentLine::isVertical()
 {
-    // XXXX
-	return true; 
+	return BasicGeometry::equal(this->_orig.getX(), this->_dest.getX());
 }
 
 double SegmentLine::slope()
 {
-	// XXXX   
-    return 0;
+	if (isVertical())
+		return std::numeric_limits<double>::infinity();
+
+	double dx = _dest.getX() - _orig.getX();
+	double dy = _dest.getY() - _orig.getY();
+
+	return dy / dx;
 }
 
 // Protected methods
